8109e806b9c7c5213cf5cb9ebe18fd2485c9f077
Daniel Soares Caldeira <soarescaldeira@outlook.com>
2023-09-04 23:18:22 -0300

    Ajuste responsividade

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-window.addEventListener("scroll", function(){
-  let header = document.querySelector('#header')
-  header.classList.toggle('rolagem', window.scrollY > 500)
-})
-
 function openMenu() {
   var element = document.getElementById("menu-celular");
+  var body = document.getElementsByTagName("body");
   if(element.classList.contains("d-none")){
-    element.classList.remove("d-none")
+    element.classList.remove("d-none");
+    body.classList.remove("");
   }else{
-    element.classList.add("d-none")
+    element.classList.add("d-none");
+    body.classList.remove("");
   } 
+
+
+
 }
 
